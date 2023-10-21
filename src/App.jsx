c9ea5fc9c6dad2654bc8585083dcffaa3070b6fd
MatheusGorga <matheusgobar@gmail.com>
2023-10-21 15:33:47 -0300

    style: create app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './global.css';
+import styles from './App.module.css';
 import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-    </>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
+    </div>
   );
 }
 
